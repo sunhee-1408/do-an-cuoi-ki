37614f2be5c0d1604dabd930b2534462c548300e
vyvy06 <brovyvy@gmail.com>
2024-12-16 22:13:01 +0700

    Update hàmkpdl.py

--- hàmkpdl.py
+++ hàmkpdl.py
@@ -9,4 +9,3 @@ def Restore_Data():
     Du_Lieu = Backup_Data.copy()
     Update_Treeview()
     messagebox.showinfo('Thông báo', 'Dữ liệu đã được khôi phục về trạng thái ban đầu!')
-Button(crud_entry, text="Khôi phục dữ liệu", bd=1, bg=background, cursor="hand2", width=19, command=Restore_Data).place(x=15, y=100)
